086cc4c1081d9d0532c1238d725d2af0190312ab
Apple <apple@Apples-MacBook-Pro-91.local>
2019-07-26 08:41:31 -0700

    useless buttons

--- emojiinall/ViewController.swift
+++ emojiinall/ViewController.swift
@@ -12,32 +12,23 @@ class ViewController: UIViewController {
 
     
     let emojis = [ "😖" : "nervous", "😹": "sad cat" ]
-
-    
+    let customMessages = [ "nervous": ["take a deep breath", "it's ok we all get nervous","don't pay attention to the other people in the room"], "sad cat":[" RELAX! it's ok", "try to think of something happy", "play with a friend / pet"] ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
     @IBAction func showMessage(sender: UIButton) {
         let selectedEmotion = sender.titleLabel?.text
-        let alertController = UIAlertController (title:" help please", message: emojis[selectedEmotion!], preferredStyle:UIAlertController.Style.alert)
+        let number = Int.random(in: 0 ... 2)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[number]
+        
+        let alertController = UIAlertController (title:" help please", message: emojiMessage,
+            preferredStyle:UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
-  
-
-
-
-
-
-
-
-
+    }
 }
-    
-    
-    
-    
-    
 
-}
+
